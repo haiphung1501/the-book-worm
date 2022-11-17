4c001416464804f6db02dc37a3f5b3827ffc73b0
PhungDucHai <haiphung15012002@gmail.com>
2022-11-17 23:08:20 +0700

    Edit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import NavBar from '../NavBar/NavBar'
+import SearchFrom from '../SearchFrom/SearchFrom'
+import "./Header.css"
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className = 'holder'>
+      <header className = 'header'>
+        <NavBar />
+        <div className = 'header-content flex flex-c text-center text-white'>
+          <h2 className = 'header-title text-capitalize'>Find your book of choice</h2><br />
+          <p className = 'header-text fs-18 fw-3' >Lorem ipsum dolor, sit amet consectetur 
+          adipisicing elit. Natus recusandae voluptate inventore nostrum doloremque tempora
+          , magnam asperiores commodi quae delectus repellendus sunt cumque eum in quasi
+           architecto alias, a dolor.  </p>
+           <SearchFrom/>
+        </div>
+      </header>
+    </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -4,11 +4,24 @@ import {
   BrowserRouter, Routes, Route
 } from 'react-router-dom';
 import './index.css';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import BookList from './components/BookList/BookList';
+import BookDetails from './components/BookDetails/BookDetails';
+import Header from './components/Header/Header';
+import Loader from './components/Loader/Loader';
+import NavBar from './components/NavBar/NavBar';
+import SearchFrom from './components/SearchFrom/SearchFrom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
+      <Route path = "/" element = {<Home/>}>
+        <Route path = "about" element = {<About/>}/>
+        <Route path = "book" element = {<BookList/>}/>
+        <Route path = "/book/:id" element = {<BookDetails/>}/>
+      </Route>
       
     </Routes>
   </BrowserRouter>
